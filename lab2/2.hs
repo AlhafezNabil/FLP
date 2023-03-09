cbe253782cd60b7327cc105b1e0a39057d8ac2bb
AlhafezNabil <nabil.alhafez@unibuc.ro>
2023-03-09 17:35:52 +0200

    Adding a new

---
+++ lab2/2.hs
@@ -0,0 +1,25 @@
+-- pa >>= k = Parser (\input -> [(b, restb) | 
+--     (a, resta) <- apply pa input,
+--     (b, restb) <- apply (k a) resta])
+
+-- instance Monad Parser where
+-- pa >>= k = Parser (\input -> [(b, restb) | 
+--     (a, resta) <- apply pa input,
+--     (b, restb) <- apply (k a) resta])
+-- instance Monad Parser where
+-- pa >>= k = Parser (\input -> [(b, restb) | 
+--     (a, resta) <- apply pa input,
+--     (b, restb) <- apply (k a) resta])
+
+
+-- cifsemn :: Parser cifSemn = do
+--     ch <- satisfy (\x -> elem x "+-")
+--     d <- digitToInt <$> digit
+--     case ch of 
+--         '+' -> return  d
+--         '-' -> return (-d)
+
+
+instance Applicative Parser where
+ pure a = Parser (\input -> [(a, input)])
+ pf <*> pa = Parser (\input -> [(f a, resta) | (f, restf) <- apply pf input, (a, resta) <- apply pa restf])
