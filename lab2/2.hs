39c5c4168db145e528af59f11249b2bd30448509
nabil.alhafez <nabil.alhfz98@gmail.com>
2023-03-15 20:49:29 +0200

    Adding lab2

--- lab2/2.hs
+++ lab2/2.hs
@@ -1,26 +1,41 @@
--- pa >>= k = Parser (\input -> [(b, restb) | 
---     (a, resta) <- apply pa input,
---     (b, restb) <- apply (k a) resta])
+import Data.Char
 
--- instance Monad Parser where
--- pa >>= k = Parser (\input -> [(b, restb) | 
---     (a, resta) <- apply pa input,
---     (b, restb) <- apply (k a) resta])
--- instance Monad Parser where
--- pa >>= k = Parser (\input -> [(b, restb) | 
---     (a, resta) <- apply pa input,
---     (b, restb) <- apply (k a) resta])
+newtype Parser' a' = Parser' { apply' :: String -> [(a', String)]}
+
+newtype Parser a = Parser { apply :: String -> [(a, String)] }
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = Parser go
+  where
+    go [] = []   -- imposibil de parsat șirul vid
+    go (c:input)
+      | p c = [(c, input)]   -- dacă predicatul ține, întoarce c și restul șirului de intrare
+      | otherwise = []       -- în caz contrar, imposibil de parsat
+
+-- exemple
+-- apply (satisfy isAlphaNum) "abc"
+-- apply (satisfy isAlphaNum) "2bc"
+-- apply (satisfy isAlphaNum) "@bc"
+
+--- | Acceptă orice caracter
+anychar :: Parser Char
+anychar = satisfy (\_ -> True)
+-- apply anychar "&ab"
 
 
--- cifsemn :: Parser cifSemn = do
---     ch <- satisfy (\x -> elem x "+-")
---     d <- digitToInt <$> digit
---     case ch of 
---         '+' -> return  d
---         '-' -> return (-d)
+--- | acceptă doar caracterul dat ca argument
+char :: Char -> Parser Char
+char x = satisfy (\c -> x == c )
+-- apply (char 'i') "ionel"
 
-newType Parser a = Parser { apply :: string -> [(a,string)]}
 
-instance Applicative Parser where
- pure a = Parser (\input -> [(a, input)])
- pf <*> pa = Parser (\input -> [(f a, resta) | (f, restf) <- apply pf input, (a, resta) <- apply pa restf])
+--- | acceptă o cifră
+digit :: Parser Char
+digit = satisfy isDigit
+
+
+
+--- | acceptă un spațiu (sau tab, sau sfârșit de linie -- vedeți funcția
+-- din Data.Char )
+space :: Parser Char
+space = undefined
